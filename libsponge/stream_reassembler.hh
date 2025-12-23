4d81508f07802e3c87196f931817b8d03e071044
倪松涛 <songtao_ni@outlook.com>
2025-12-23 15:27:47 +0800

    复习善后

--- libsponge/stream_reassembler.hh
+++ libsponge/stream_reassembler.hh
@@ -1,70 +1,76 @@
 #ifndef SPONGE_LIBSPONGE_STREAM_REASSEMBLER_HH
 #define SPONGE_LIBSPONGE_STREAM_REASSEMBLER_HH
 
-#include "byte_stream.hh"
-
 #include <cstdint>
+#include <optional>
 #include <set>
 #include <string>
 
-//! \brief A class that assembles a series of excerpts from a byte stream (possibly out of order,
-//! possibly overlapping) into an in-order byte stream.
+#include "byte_stream.hh"
+
+//! \brief A class that assembles a series of excerpts from a byte stream
+//! (possibly out of order, possibly overlapping) into an in-order byte stream.
 class StreamReassembler {
-  private:
-    // Your code here -- add private members as necessary.
-    struct buffer_node {
-        size_t _first_index = 0;
-        std::string data{};
-        bool operator<(const buffer_node &other) const { return _first_index < other._first_index; }
-    };
-    std::set<buffer_node> _buffers{};
-    size_t _unassembled_bytes = 0;
-    bool _eof = false;
-    ByteStream _output;  //!< The reassembled in-order byte stream
-    size_t _capacity;    //!< The maximum number of bytes
+ private:
+  // Your code here -- add private members as necessary.
+  struct Segment {
+    size_t _first_index = 0;
+    std::string data{};
+    bool operator<(const Segment& other) const {
+      return _first_index < other._first_index;
+    }
+  };
+  std::set<Segment> _buffers{};
+  size_t _unassembled_bytes = 0;
+  bool _eof = false;
+  ByteStream _output;  //!< The reassembled in-order byte stream
+  size_t _capacity;    //!< The maximum number of bytes
 
-    size_t available_capacity();
+  size_t available_capacity();
 
-    long merge_data(buffer_node &elem1, const buffer_node &elem2);
+  long merge_data(Segment& elem1, const Segment& elem2);
 
-  public:
-    size_t first_unread();
+ public:
+  size_t first_unread();
 
-    size_t first_unacceptable();
+  size_t first_unacceptable();
 
-    size_t first_unassembled();
-    //! \brief Construct a `StreamReassembler` that will store up to `capacity` bytes.
-    //! \note This capacity limits both the bytes that have been reassembled,
-    //! and those that have not yet been reassembled.
-    StreamReassembler(const size_t capacity);
+  size_t first_unassembled();
+  //! \brief Construct a `StreamReassembler` that will store up to `capacity`
+  //! bytes.
+  //! \note This capacity limits both the bytes that have been reassembled,
+  //! and those that have not yet been reassembled.
+  StreamReassembler(const size_t capacity);
 
-    //! \brief Receives a substring and writes any newly contiguous bytes into the stream.
-    //!
-    //! If accepting all the data would overflow the `capacity` of this
-    //! `StreamReassembler`, then only the part of the data that fits will be
-    //! accepted. If the substring is only partially accepted, then the `eof`
-    //! will be disregarded.
-    //!
-    //! \param data the string being added
-    //! \param index the index of the first byte in `data`
-    //! \param eof whether or not this segment ends with the end of the stream
-    void push_substring(const std::string &data, const uint64_t index, const bool eof);
+  //! \brief Receives a substring and writes any newly contiguous bytes into the
+  //! stream.
+  //!
+  //! If accepting all the data would overflow the `capacity` of this
+  //! `StreamReassembler`, then only the part of the data that fits will be
+  //! accepted. If the substring is only partially accepted, then the `eof`
+  //! will be disregarded.
+  //!
+  //! \param data the string being added
+  //! \param index the index of the first byte in `data`
+  //! \param eof whether or not this segment ends with the end of the stream
+  void push_substring(const std::string& data, const uint64_t index,
+                      const bool eof);
 
-    //! \name Access the reassembled byte stream
-    //!@{
-    const ByteStream &stream_out() const { return _output; }
-    ByteStream &stream_out() { return _output; }
-    //!@}
+  //! \name Access the reassembled byte stream
+  //!@{
+  const ByteStream& stream_out() const { return _output; }
+  ByteStream& stream_out() { return _output; }
+  //!@}
 
-    //! The number of bytes in the substrings stored but not yet reassembled
-    //!
-    //! \note If the byte at a particular index has been submitted twice, it
-    //! should only be counted once for the purpose of this function.
-    size_t unassembled_bytes() const;
+  //! The number of bytes in the substrings stored but not yet reassembled
+  //!
+  //! \note If the byte at a particular index has been submitted twice, it
+  //! should only be counted once for the purpose of this function.
+  size_t unassembled_bytes() const;
 
-    //! \brief Is the internal state empty (other than the output stream)?
-    //! \returns `true` if no substrings are waiting to be assembled
-    bool empty() const;
+  //! \brief Is the internal state empty (other than the output stream)?
+  //! \returns `true` if no substrings are waiting to be assembled
+  bool empty() const;
 };
 
 #endif  // SPONGE_LIBSPONGE_STREAM_REASSEMBLER_HH
--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -6,62 +6,69 @@
 // automated checks run by `make check_lab2`.
 
 template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
+void DUMMY_CODE(Targs&&... /* unused */) {}
 
 using namespace std;
 
-bool TCPReceiver::segment_received(const TCPSegment &seg) {
-    bool is_syn_fin = false;
-    static size_t abs_seq = 0;
-    size_t length = 0;
-    if (seg.header().syn) {
-        if (_sync_flag) {  // 已经接收过了sync
-            return false;
-        }
-        _sync_flag = true;
-        is_syn_fin = true;
-        _isn = seg.header().seqno.raw_value();
-        abs_seq = 1;
-        _base = 1;
-        length = seg.length_in_sequence_space();
-        if (length == 0) {  // 没有负载信息
-            return true;
-        }
-    } else if (!_sync_flag) {
-        return false;
-    } else {  // 普通数据段
-        abs_seq = unwrap(seg.header().seqno, WrappingInt32(_isn), abs_seq);
-        length = seg.length_in_sequence_space();
+bool TCPReceiver::segment_received(const TCPSegment& seg) {
+  bool is_syn_fin = false;
+  size_t abs_seq = 0;
+  size_t length = 0;
+  if (seg.header().syn) {
+    if (_sync_flag) {  // 已经接收过了sync
+      return false;
     }
+    _sync_flag = true;
+    is_syn_fin = true;
+    _isn = seg.header().seqno.raw_value();
+    abs_seq = 1;
+    _base = 1;
+    length = seg.length_in_sequence_space() - 1;
+    if (length == 0) {  // 没有负载信息
+      return true;
+    }
+  } else if (!_sync_flag) {
+    return false;
+  } else {  // 普通数据段
+    abs_seq = unwrap(seg.header().seqno, WrappingInt32(_isn), abs_seq);
+    length = seg.length_in_sequence_space();
+  }
 
-    if (seg.header().fin) {
-        if (_fin_flag) {
-            return false;
-        }
-        _fin_flag = true;
-        is_syn_fin = true;
-    } else if (length == 0 && abs_seq == _base) {
-        return true;
-    } else if (abs_seq + length <= _base || abs_seq >= _base + window_size()) { //注意条件，比如seq = 3，len = 3，_base为6的情况，seq = 6是取不到的
-        if (!is_syn_fin) {
-            return false;
-        }
+  if (seg.header().fin) {
+    if (_fin_flag) {
+      return false;
     }
-
-    _reassembler.push_substring(seg.payload().copy(), abs_seq - 1, seg.header().fin);
-    _base = _reassembler.first_unassembled() + 1;
-    if (seg.header().fin) {
-        ++_base;
-    }
+    _fin_flag = true;
+    is_syn_fin = true;
+  } else if (length == 0 && abs_seq == _base) {
     return true;
+  } else if (abs_seq + length <= _base ||
+             abs_seq >=
+                 _base +
+                     window_size()) {  // 注意条件，比如seq = 3，len =
+                                       // 3，_base为6的情况，seq = 6是取不到的
+    if (!is_syn_fin) {
+      return false;
+    }
+  }
+
+  _reassembler.push_substring(seg.payload().copy(), abs_seq - 1,
+                              seg.header().fin);
+  _base = _reassembler.first_unassembled() + 1;
+  if (seg.header().fin) {
+    ++_base;
+  }
+  return true;
 }
 
 optional<WrappingInt32> TCPReceiver::ackno() const {
-    if (_base > 0) {
-        return WrappingInt32(wrap(_base, WrappingInt32(_isn)));
-    } else {
-        return nullopt;
-    }
+  if (_base > 0) {
+    return WrappingInt32(wrap(_base, WrappingInt32(_isn)));
+  } else {
+    return nullopt;
+  }
 }
 
-size_t TCPReceiver::window_size() const { return _capacity - _reassembler.stream_out().buffer_size(); }
+size_t TCPReceiver::window_size() const {
+  return _capacity - _reassembler.stream_out().buffer_size();
+}
